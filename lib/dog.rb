fb3f535d5b50e12b246207acfdb8d1f7672f26cd
Tyler Hunt <tyhunt04@gmail.com>
2019-02-12 17:24:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,31 +1,14 @@
 class Dog
-  def initialize(dogs_name, dogs_breed)
-    @name = dogs_name
-    @breed = dogs_breed
-  end
-
-  def name=(dogs_name)
-    @name = dogs_name
+  def initialize(name)
+    @name = name
   end
 
   def name
     @name
   end
 
-  def breed=(dogs_breed)
-    @breed = dogs_breed
+  def name= (dogs_name)
+    @name = dogs_name
   end
-
-  def breed
-    @breed
-  end
+  
 end
-
-fido = Dog.new
-fido.name = "Fido"
-puts fido.name
-
-snoopy = Dog.new
-snoopy.name = "Snoopy"
-snoopy.breed = "Beagle"
-puts snoopy.name
